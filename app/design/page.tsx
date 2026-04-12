553433dfc4c5bb058ba5d82680ea0b1217639294
111601038-a11y <111601038@g.nccu.edu.tw>
2026-04-12 22:31:50 +0800

    feat:all update

---
+++ app/design/page.tsx
@@ -0,0 +1,23 @@
+"use client"
+import Image from "next/image"
+import Menu from "../../component/Menu"
+import Link from "next/link";
+export default function about() {
+
+  return (
+    <>
+      <div className="flex h-full">
+        <Link href="/" className="sm:hidden absolute left-2 top-2 bg-white">  
+            返回
+        </Link>
+        <div className="sm:block hidden">
+          <Menu />
+        </div>
+        <div className="bg-red-500 w-full">
+          設計專案
+        </div>
+      </div>
+      
+    </>
+  );
+}
